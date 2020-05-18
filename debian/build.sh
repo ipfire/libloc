405070361f1190b465e9356ddbd47c33ffcce93f
Michael Tremer <michael.tremer@ipfire.org>
2020-05-18 14:54:24 +0000

    debian: Add script to build package for various arches
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

---
+++ debian/build.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -x
+
+ARCHITECTURES=( amd64 arm64 i386 armhf riscv64 )
+RELEASES=( buster )
+
+CHROOT_PATH="/var/tmp"
+
+main() {
+    if [ $# -lt 2 ]; then
+        echo "Not enough arguments" >&2
+        return 2
+    fi
+
+    local package="${1}"
+    local sources="${2}"
+
+    # Create some temporary directory
+    local tmp="$(mktemp -d)"
+
+    # Extract the sources into it
+    tar xvfa "${sources}" -C "${tmp}"
+
+    # Copy the tarball under the correct Debian name
+    cp -vf "${sources}" "${tmp}/${package//-/_}.orig.tar.xz"
+
+    # Change into source directory
+    pushd "${tmp}/${package}"
+
+    # Prepare the build environment
+    #if ! debuild -us -uc; then
+    #    echo "Could not prepare build environment" >&2
+    #    return 1
+    #fi
+
+    # Build the package for each release
+    local release
+    for release in ${RELEASES[@]}; do
+        # And for each architecture we want to support
+        local arch
+        for arch in ${ARCHITECTURES[@]}; do
+            local chroot="${release}-${arch}-sbuild"
+
+            # Create a chroot environment
+            if [ ! -d "/etc/sbuild/chroot/${chroot}" ]; then
+                if ! sbuild-createchroot --arch="${arch}" "${release}" \
+                        "${CHROOT_PATH}/${chroot}"; then
+                    echo "Could not create chroot for ${release} on ${arch}" >&2
+                    return 1
+                fi
+            fi
+
+            # Run the build process
+            if ! sbuild --dist="${release}" --host="${arch}"; then
+                echo "Could not build package for ${release} on ${arch}" >&2
+                return 1
+            fi
+        done
+    done
+
+    popd
+
+    # Cleanup
+    rm -rf "${tmp}"
+}
+
+main "$@" || exit $?
