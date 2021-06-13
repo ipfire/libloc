af8ba4287e3afac16b7a8e29f22355143fe6661d
Valters Jansons <valter.jansons@gmail.com>
2021-06-13 19:16:25 +0300

    debian: Attribute all maintainers in changlog
    This commit further builds on historical changelog modifications,
    to properly attribute all authors of the commits.
    
    An additional d/genchangelog.sh script has been added. This allows
    generation of changelog entries, internally using `debchange` (`dch`).
    The script accepts an argument, which is the commit range to generate
    entries for. Each commit's subject line (first line of body) is used,
    along with author name and email. This information is added to the
    changelog. Automatic detection (via `debchange` built-in functionality)
    is used to determine whether these entries should be added to an
    existing version number. If there is no UNRELEASED version, then a new
    version is automatically tagged.
    
    The new version tag will usually need to be modified, for example,
    replacing an automatically generated 0.9.6-2 with 0.9.7-1.
    
    The final release change (s/UNRELEASED/unstable/) needs to be done
    manually as well, when the Git tag is actually being tagged.
    `dch -r` can be useful for this particular purpose.
    
    Signed-off-by: Valters Jansons <valter.jansons@gmail.com>
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

---
+++ debian/genchangelog.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+gitshow () {
+  local format=$1
+  local commit=$2
+
+  git show --no-patch --format=format:"$format" "$commit"
+}
+
+main () {
+  if [ $# -lt 1 ]; then
+    local bn="$(basename $0)"
+    echo "Usage:    $bn  <commit range>" >&2
+    echo "Example:  $bn  0.9.7..HEAD" >&2
+    echo "Example:  $bn  0.9.5..0.9.6^" >&2
+    return 1
+  fi
+
+  local commitrange=$1
+
+  local commit
+  for commit in $(git rev-list --reverse "$commitrange"); do
+    # Skip commits with diffs that only have Makefile.am or d/ changes.
+    if [ "$(git diff --name-only "${commit}^..${commit}" -- . ':^Makefile.am' ':^debian/' | wc -l)" == 0 ]; then
+      continue
+    fi
+
+    local author_name="$(gitshow %an "$commit")"
+    local author_email="$(gitshow %ae "$commit")"
+    local subject="$(gitshow %s "$commit")"
+
+    echo "$author_name <$author_email>  $subject"
+    DEBFULLNAME="$author_name" DEBEMAIL="$author_email" debchange --upstream --multimaint-merge "$subject"
+  done
+}
+
+main "$@" || exit $?
