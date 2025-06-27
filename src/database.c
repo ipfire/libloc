ipfire/libloc

commit 9e72b8a0c56403ca7d23c6a767d66038e0d63c14

database: Have the lookup function return 0 even if nothing was found
This is a potentially breaking change, but I think we need to do this as
the previous return code was not very descriptive.

Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

src/database.c
@@ -924,11 +924,12 @@ static int __loc_database_lookup(struct loc_database* db, const struct in6_addr*
 	// If this node has a leaf, we will check if it matches
 	if (__loc_database_node_is_leaf(node_v1)) {
 		r = __loc_database_lookup_handle_leaf(db, address, network, network_address, level, node_v1);
-		if (r <= 0)
+		if (r < 0)
 			return r;
 	}
 
-	return 1;
+	// Return no error - even if nothing was found
+	return 0;
 }
 
 LOC_EXPORT int loc_database_lookup(struct loc_database* db,

src/lua/database.c
@@ -202,8 +202,10 @@ static int Database_lookup(lua_State* L) {
 	}
 
 	// Create a network object
-	r = create_network(L, network);
-	loc_network_unref(network);
+	if (network) {
+		r = create_network(L, network);
+		loc_network_unref(network);
+	}
 
 	return r;
 }

src/python/database.c
@@ -201,35 +201,35 @@ static PyObject* Database_get_country(DatabaseObject* self, PyObject* args) {
 static PyObject* Database_lookup(DatabaseObject* self, PyObject* args) {
 	struct loc_network* network = NULL;
 	const char* address = NULL;
+	int r;
 
 	if (!PyArg_ParseTuple(args, "s", &address))
 		return NULL;
 
 	// Try to retrieve a matching network
-	int r = loc_database_lookup_from_string(self->db, address, &network);
+	r = loc_database_lookup_from_string(self->db, address, &network);
+	if (r) {
+		// Handle any errors
+		switch (errno) {
+			case EINVAL:
+				PyErr_Format(PyExc_ValueError, "Invalid IP address: %s", address);
 
-	// We got a network
-	if (r == 0) {
-		PyObject* obj = new_network(&NetworkType, network);
-		loc_network_unref(network);
+			default:
+				PyErr_SetFromErrno(PyExc_OSError);
+		}
 
-		return obj;
+		return NULL;
 	}
 
 	// Nothing found
-	if (!errno)
+	if (!network)
 		Py_RETURN_NONE;
 
-	// Handle any errors
-	switch (errno) {
-		case EINVAL:
-			PyErr_Format(PyExc_ValueError, "Invalid IP address: %s", address);
+	// We got a network
+	PyObject* obj = new_network(&NetworkType, network);
+	loc_network_unref(network);
 
-		default:
-			PyErr_SetFromErrno(PyExc_OSError);
-	}
-
-	return NULL;
+	return obj;
 }
 
 static PyObject* new_database_enumerator(PyTypeObject* type, struct loc_database_enumerator* enumerator) {

src/test-network.c
@@ -359,7 +359,7 @@ int main(int argc, char** argv) {
 
 	// Lookup an address outside the subnet
 	err = loc_database_lookup_from_string(db, "2001:db8:fffe:1::", &network1);
-	if (err == 0) {
+	if (err || network1) {
 		fprintf(stderr, "Could look up 2001:db8:fffe:1::, but I shouldn't\n");
 		exit(EXIT_FAILURE);
 	}

commit 95d271bb90b4cebe81c288a95a2336aa63bac4d7

database: Fix backtracking after no match was found at the end of the tree
This is a rare bug which only appears if the tree has not been 100%
optimised which might happen because it would not be worth the effort to
remove all dead ends all of the time.

Therefore, we could be reaching a node which is not an actual leaf but
still not find our network. In that case, we will have to go backwards
slightly to check if there has been a less specific match (but a match
nonetheless).

This was broken in commit 9e72b8a0c56403ca7d23c6a767d66038e0d63c14 when
the return code for no match was changed to zero.

Fixes: #13861 - libloc-0.9.18 fails to find some ASN info
Reported-by: Adolf Belka <adolf.belka@ipfire.org>
Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

src/database.c
@@ -907,13 +907,7 @@ static int __loc_database_lookup(struct loc_database* db, const struct in6_addr*
 
 		// Move on to the next node
 		r = __loc_database_lookup(db, address, network, network_address, node_index, level + 1);
-
-		// End here if a result was found
-		if (r == 0)
-			return r;
-
-		// Raise any errors
-		else if (r < 0)
+		if (r < 0)
 			return r;
 
 		DEBUG(db->ctx, "No match found below level %u\n", level);
@@ -922,7 +916,7 @@ static int __loc_database_lookup(struct loc_database* db, const struct in6_addr*
 	}
 
 	// If this node has a leaf, we will check if it matches
-	if (__loc_database_node_is_leaf(node_v1)) {
+	if (!*network && __loc_database_node_is_leaf(node_v1)) {
 		r = __loc_database_lookup_handle_leaf(db, address, network, network_address, level, node_v1);
 		if (r < 0)
 			return r;